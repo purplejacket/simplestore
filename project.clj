d916e9ff70ec53c83b68928a8c48a6e5cb7c6b8f
Kai Middleton <kai.middleton@gmail.com>
2014-12-14 14:14:01 -0800

    built skeleton project with lein new

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject simplestore "0.1.0-SNAPSHOT"
+  :description "Something like a simple key value store"
+  :url "https://github.com/purplejacket/simplestore"
+  :dependencies [[org.clojure/clojure "1.6.0"]])
