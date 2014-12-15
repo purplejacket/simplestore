340d40c10f017ca85b5ec9d96786805172cca0b6
Kai Middleton <kai.middleton@gmail.com>
2014-12-14 16:34:48 -0800

    can now create entries, w00t

--- src/simplestore/crud.clj
+++ src/simplestore/crud.clj
@@ -1,7 +1,16 @@
 (ns simplestore.crud)
 
 (def datastore
-  (ref {"key1" "val1"}))
+  (ref {}))
 
 (defn read [key]
   (get @datastore key))
+
+;; TODO - decide if create should blow up on trying
+;; to put in a key-val where key already exists
+(defn create [key val]
+  (dosync
+    (alter datastore
+           (fn [refvalue]
+             (assoc refvalue key val))))
+  true)
--- test/simplestore/core_test.clj
+++ test/simplestore/core_test.clj
@@ -2,7 +2,16 @@
   (:require [clojure.test :refer :all]
             [simplestore.crud :as crud]))
 
-(deftest a-test
+(deftest test-read
   (testing "it reads a value"
-    (is (= (crud/read "key1") "val1"))))
+    (dosync (ref-set crud/datastore {"key1" "val1"}))
+    (is (= (crud/read "key1") "val1"))
+    (is (= @crud/datastore {"key1" "val1"}))))
+
+(deftest test-create
+  (testing "it creates a value"
+    (dosync (ref-set crud/datastore {"key1" "val1"}))
+    (is (= (crud/create "key2" "val2") true))
+    (is (= @crud/datastore {"key1" "val1"
+                            "key2" "val2"}))))
 
